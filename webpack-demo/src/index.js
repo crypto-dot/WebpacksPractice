5ad3cae02a5be6d912ea6c807c59736ff3708ee6
Carlos Arbizu <carlos2345532@gmail.com>
2022-04-05 15:55:47 -0500

    finished

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,7 +2,20 @@
 import myName from './myName';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './research-and-development-survey-2016-2019-csv-notes.csv'
+import yaml from './data.yaml';
+import json from './data.json5';
+import toml from './data.toml';
 
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement("div");
@@ -15,6 +28,10 @@ function component() {
     const myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+    console.log(Data);
+    console.log(Notes);
     return element;
-}
+
+
+    }
 document.body.appendChild(component());
