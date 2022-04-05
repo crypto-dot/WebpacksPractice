c9e08e820d2d7deaef39c8ce76ba8033cb6c9800
Carlos Arbizu <carlos2345532@gmail.com>
2022-04-05 14:52:43 -0500

    Followed webpack's beginning tutorial on setting up config files along with bundling js files

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement("div");
+
+    //Lodash, now imported by this script
+    element.textContent = myName("Cody");
+
+    return element;
+}
+document.body.appendChild(component());
