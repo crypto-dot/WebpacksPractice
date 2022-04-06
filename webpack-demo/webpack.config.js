f1add6f6fb9916e5399fff279e03c779bae06e24
Carlos Arbizu <carlos2345532@gmail.com>
2022-04-06 13:12:56 -0500

    Completed output management in webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
-
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        index: "./src/index.js",
+        print: "./src/print.js",
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean:true,
+        publicPath: '/',
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader','css-loader']
-            },
-            {
-                test: /\.(png|jpg|svg|gif|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test:/\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
+    
 };
